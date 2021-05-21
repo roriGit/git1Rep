149e2b7b0cd639a2dbcd6d4b2996585650616123
Rorisang Madonko <rori.madonko@gmail.com>
2021-05-21 10:05:33 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<h1>1,2 Testing.</h1>
+<h2>Hello world.</h2>
+<script type="text/javascript" src="main.js"></script>
+</body>
+</html>
